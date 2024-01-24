597ac2f4acb2341286ebdbce612dd44b2e393e91
Emily Liang <exliang@uci.edu>
2024-01-23 22:21:21 -0800

    changes

---
+++ wp1.py
@@ -0,0 +1,20 @@
+
+#wp1.py
+
+#Emily Liang
+#exliang@uci.edu
+#79453973
+
+import sys 
+
+def main():
+	pass
+
+def reverse_cipher():
+	#read in data from a txt file
+	#put each line in file into list, reverse the list & use string slicing to reverse each sentence
+	#output data in another file
+	
+
+if __name__ == '__main__':
+	main()
