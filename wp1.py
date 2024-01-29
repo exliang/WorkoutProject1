da1b2e4e77b839c488064253e90bed605e29b642
exliang <156244062+exliang@users.noreply.github.com>
2024-01-28 22:56:34 -0800

    "wp1 loop edited"

--- wp1.py
+++ wp1.py
@@ -16,49 +16,43 @@ def reverse_cipher():
 
 	input_file = open(file_input, 'rb')
 	output_file = open(file_output, 'wb')
+
+	print("input_file.read():", input_file.read())
 	input_file.seek(0,2)  # move file pointer to end of file
-	current_pos = input_file.tell()
-	index = -1
+	end = input_file.tell()  # current pos of file pointer
+	index = 0  # char count
 
-	while current_pos >= 0:  # a condition thats True as long as there are more bytes to read 
-		# print("input_file.read(1):",input_file.read(1))
-		output_file.write(input_file.read(1))
-
-		# if input_file.read(1) == b"\r" or input_file.read(1) == b"\n" or input_file.read(1) == b"\r\r" or input_file.read(1) == b"\r\n" or input_file.read(1) == b"\n\n":
-		# 	# write to file but w/out the newline
-		# 	print("CARRIAGE RETURN FOUND")
-		# 	continue
-		# else:
-		# 	output_file.write(input_file.read(1))
-
-		if current_pos >= 2:  # if there's more than 2 chars left in file to read 
-			input_file.seek(index,2)  # move file pointer to the indexth char from end of file
-		else:
-			# print("else seek(0)")
-			input_file.seek(0)
-		# print("current_pos after moving pointer by 1:", current_pos)
-		# print("current index after moving pointer by 1:", index)
-		current_pos -= 1  # decrease current position
-		index -= 1
-	# output_file.write(input_file.read(1))  # writing the first letter 
-
+	while index <= end:  # loops when char count <= total chars in file   
+		char1 = read_char(input_file)
+		index += 1
+		if index <= end:  # if char count <= total chars in file (calls this everytime before .seek)
+			input_file.seek(-1,1) #moves file pointer to left 1
+			char2 = read_char(input_file)
+			print("char2:",char2)
+			if char1 == b'\n' and char2 == b'\r':
+				output_file.write(b'\r\n')
+				index += 1
+				if index <= end:
+					input_file.seek(-1,1) #moves file pointer to left 1 again
+			else:
+				output_file.write(char1)  # if it's not \n & \r just write a single char
+		else:  # 1 char left 
+			output_file.write(char1)  # if it's not \n & \r just write a single char
+ 
 	input_file.close()
 	output_file.close()
+
+
+def read_char(input_file):  # reads char w/out changing position of file pointer
+	# keeps track of file's current pos, reads, goes back to current pos
+	current_pos = input_file.tell()
+	char = input_file.read(1)
+	input_file.seek(current_pos)  # moves file to current pos after reading 
+	return char
+
 
 if __name__ == '__main__':
 	main()
 
 # Citations:
 # - https://pynative.com/python-file-seek/#h-seek-backward-with-negative-offset
-
-	# output_file = open(file_output, 'a+')
-	# line_num = 1
-	# for line in output_file:
-	# 	if line_num % 2 == 0:  # even
-	# 		line.
-	# 	line_num += 1
-
-		# if input_file.read(1) == "\n":
-		# 	# write to file but w/out the newline
-		# 	print("NEWLINE read")
-		# else:
